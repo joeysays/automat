de7ca1c7839bfda2aea329883538bedab90b6a3e
Joey Sayson <jose_vicente_sayson@dlsu.edu.ph>
2018-08-12 22:27:45 -0700

    inital codes

---
+++ AutomatMP1/src/automatmp1/Earth.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatmp1;
+
+import java.util.ArrayList;
+//import javax.swing.text.html.parser.Entity;
+
+/**
+ *
+ * @author Belle
+ */
+public class Earth {
+    Formatter formatter;
+    ArrayList<Entity> entity;
+    
+//    Entity e = new Entity("Scientist", "Earth");
+//    formatter.appendToPane(jTextPane1, "Passenger #" + e.name, color.blue);
+}
---
+++ AutomatMP1/src/automatmp1/Entity.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatmp1;
+
+/**
+ *
+ * @author Belle
+ */
+public class Entity {
+    public String name;
+    public String location;
+    
+    public Entity(String name, String location){
+        this.name = name;
+        this.location = location;
+    }
+}
